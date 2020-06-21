e7da08252a76bed2a869e8264ce619714305dd22
pedraodeveloper66 <phdesenvolvedor@yahoo.com>
2020-06-21 18:54:45 +0000

    Added sum on a separate line

--- solutions/solved0003.py
+++ solutions/solved0003.py
@@ -1,8 +1,9 @@
 """
 """
 
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite mais um número: '))
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
 
-print('A soma entre {} e {} vale {}'.format(n1, n2, n1+n2))
+print('A soma entre {} e {} é igual a {}!'.format(n1, n2, s))
     
