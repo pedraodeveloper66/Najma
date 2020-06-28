1f5a9ef8ef46f3bb5160e78c381b686a2c5e5dd6
pedraodeveloper66 <phdesenvolvedor@yahoo.com>
2020-06-28 19:27:34 +0000

    Solved

--- solutions/solved005.py
+++ solutions/solved005.py
@@ -1,3 +1,6 @@
 """
 Create an algorithm that reads a number and shows its double, triple and square root.
 """
+number = int(input('Digite um número: '))
+
+print('Analisando o valor {0}, seu antecessor é {1} e o sucessor é {2}'.format(number, number - 1, number + 1))
