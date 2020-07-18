80a19a635ac0500d432de251c6e210e1875ed4d3
pedraodeveloper66 <phdesenvolvedor@yahoo.com>
2020-07-18 21:22:56 +0000

    Successfully concluded

--- solutions/solved006.py
+++ solutions/solved006.py
@@ -1,3 +1,5 @@
 """
-Make a program that reads a whole number and shows your successor and predecessor on the screen.
+Create an algorithm that reads a number and shows its double, triple and square root.
 """
+n = int(input('Digite um número: '))
+print('O doblo de {} vale {}.\nO triplo de {} vale {}.\nA raiz quadrada de {} é igual a {:.2f}.'.format(n, n*2, n, n*3, n, float(n**0.5)))
