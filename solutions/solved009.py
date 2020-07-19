73947aebef0632d59356fe19b9761ed1ab420797
pedraodeveloper66 <phdesenvolvedor@yahoo.com>
2020-07-19 18:32:56 +0000

    Doing some tests

--- solutions/solved009.py
+++ solutions/solved009.py
@@ -1,3 +1,15 @@
 """
 Make a program that reads any integer and shows your multiplication table on the screen.
 """
+
+num = int(input('Digite um número para ver sua tabuada: '))
+
+print('-' * 12)
+
+print(5 * ' ' + 'TABUADA {}'.format(num) + ' ' *5)  
+
+for i in range(10):
+    print('{:2} x {} = {:2}\n'.format(i+1, num, num*(i+1)))
+
+print('-' * 12)
+    
