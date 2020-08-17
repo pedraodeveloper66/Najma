a5fd9892f65ecfcd230ccefe82d200ca256863df
pedraodeveloper66 <phdesenvolvedor@yahoo.com>
2020-08-17 01:21:40 +0000

    new changes

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -1,5 +1,7 @@
 FROM gitpod/workspace-full
 
+RUN python -m pip install emoji
+
 # Install custom tools, runtimes, etc.
 # For example "bastet", a command-line tetris clone:
 # RUN brew install bastet
---
+++ solutions/solved016.py
@@ -0,0 +1,15 @@
+"""
+Create a program that reads any Real number by keyboard and shows on the screen its entire portion.
+
+Example:
+Enter a number: 6.127
+Number 6.127 has the whole part 6.
+
+math.trunc(x) 	Returns the truncated integer parts of x
+"""
+
+from math import trunc
+
+print('{}'.format(trunc(9.1)))
+    
+
